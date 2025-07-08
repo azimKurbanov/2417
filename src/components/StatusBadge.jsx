f52d683818af5e7e59a95faa6b8269836f655099
azimKurbanov <azim.kurbanov.2000@mail.ru>
2025-07-08 18:58:41 +0500

    fsdfdsdfs

--- src/components/StatusBadge.jsx
+++ src/components/StatusBadge.jsx
@@ -5,7 +5,9 @@ const StatusBadge = ({ status }) => {
     const active = "bg-green-100 text-green-700";
     const inactive = "bg-red-100 text-red-700";
     return <span className={`${base} ${status === "Active" ? active : inactive}`}>{status}</span>;
+    <h2>BDlAZ</h2>
   };
   <h1>salom</h1>
+ 
   export default StatusBadge;
   
